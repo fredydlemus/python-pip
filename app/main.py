60dadca89081eb71b44239afca4b5953c5c50895
Fredy Daniel Flores Lemus <56806728+fredydlemus@users.noreply.github.com>
2023-02-24 07:15:26 -0600

    add more projects

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
-    data = read_csv.read_csv('data.csv')
-    data = list(
-        filter(lambda item: item['Continent'] == 'South America', data))
+    # data = read_csv.read_csv('data.csv')
+    # data = list(
+    #     filter(lambda item: item['Continent'] == 'South America', data))
 
-    countries = list(map(lambda x: x['Country'], data))
-    percentages = list(map(lambda x: x['World Population Percentage'], data))
+    # countries = list(map(lambda x: x['Country'], data))
+    # percentages = list(map(lambda x: x['World Population Percentage'], data))
+
+    # charts.generate_pie_chart(countries, percentages)
+
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'Africa']
+
+    countries = df['Country'].values
+    percentages = df['World Population Percentage'].values
 
     charts.generate_pie_chart(countries, percentages)
 
+    data = read_csv.read_csv('data.csv')
     country = input('Type Country => ')
     print(country)
 
