a982b0fe94fd0dc08fce5455e785b5b861a35c52
Fredy Daniel Flores Lemus <56806728+fredydlemus@users.noreply.github.com>
2023-02-25 12:43:19 -0600

    add docker

--- app/charts.py
+++ app/charts.py
@@ -12,7 +12,7 @@ def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.savefig('pie.png')
+    plt.savefig('charts_pie.png')
     plt.close()
 
 
